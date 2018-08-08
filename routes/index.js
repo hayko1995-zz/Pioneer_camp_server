1181964cb801a5a92846c0da6a624f9db388cea7
Hayko1995 <Haykhakobyan95@gmail.com>
2018-08-08 16:14:43 +0400

    esim inch

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello' });
 });
 
+router.post('/', function (req, res) {
+  //var stmt = db.prepare("INSERT INTO user VALUES (?,?)");
+  console.log(req.body);
+ 
+  //stmt.run(req.body.firstname, req.body.lastname)
+  
+});
 module.exports = router;
